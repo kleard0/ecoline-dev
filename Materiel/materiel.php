18b8d25ef88a66a8bc5087f789c2cfd3441e1318
Armand <149439396+Wharra@users.noreply.github.com>
2024-05-19 15:33:43 +0200

    voir stock update

--- Materiel/materiel.php
+++ Materiel/materiel.php
@@ -12,17 +12,11 @@ if (!$connexion) {
 
 $requete_produits = "SELECT * FROM produits";
 $resultat_produits = $connexion->query($requete_produits);
-$requete_history = "SELECT * FROM history";
-$resultat_history = $connexion->query($requete_history);
 
 
 $donnees_produits = array();
 while ($ligne_produits = $resultat_produits->fetch_assoc()) {
     $donnees_produits[] = $ligne_produits;
-}
-$donnees_history = array();
-while ($ligne_history = $resultat_history->fetch_assoc()) {
-    $donnees_history[] = $ligne_history;
 }
 
 
@@ -45,6 +39,7 @@ while ($ligne_history = $resultat_history->fetch_assoc()) {
         foreach ($ligne_produits as $valeur_produits) {
             echo "<td>" . $valeur_produits . "</td>";
         }
+        echo "<button>supprimer</button>";
         echo "</tr>";
     }
 ?>
