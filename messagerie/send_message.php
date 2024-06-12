728508cf34a70528a977fdea323faab8fa7f9981
Zedasix <64897364+Zedasix@users.noreply.github.com>
2024-06-12 21:39:34 +0200

    message terminé
    yippee !

--- messagerie/send_message.php
+++ messagerie/send_message.php
@@ -19,34 +19,33 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $destinataire_id = $_POST['destinataire_id']; // Destinataire
     $message_content = $_POST['message_content'];
     $message_text = $_POST['message_text'];
-    $message_media = "";
-
+    
     // Gestion de l'upload de fichier
     if (!empty($_FILES['message_media']['name'])) {
-        $target_dir = "uploads/";
-        $target_file = $target_dir . basename($_FILES["message_media"]["name"]);
-        if (move_uploaded_file($_FILES["message_media"]["tmp_name"], $target_file)) {
-            $message_media = $target_file;
+        $file_name_with_extension = $_FILES['message_media']['name'];
+        $file_name = pathinfo($file_name_with_extension, PATHINFO_FILENAME);
+        $file_extension = pathinfo($file_name_with_extension, PATHINFO_EXTENSION);
+        $message_media = file_get_contents($_FILES['message_media']['tmp_name']); // Contenu du fichier
+
+        // Insérez le contenu du fichier, le nom et l'extension dans la base de données
+        $sql = "INSERT INTO message (user_id, destinataire_id, message_content, message_text, message_media, name_file, name_extension) 
+                VALUES (?, ?, ?, ?, ?, ?, ?)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("iisssss", $user_id, $destinataire_id, $message_content, $message_text, $message_media, $file_name, $file_extension);
+
+        if ($stmt->execute()) {
+            echo "<div>Message envoyé. Vous serez redirigé dans 2 secondes...</div>";
+            echo "<script>
+                    setTimeout(function(){
+                        window.location.href = 'messages.php';
+                    }, 2000);
+                  </script>";
+        } else {
+            echo "Erreur: " . $sql . "<br>" . $conn->error;
         }
+
+        $stmt->close();
     }
-
-    $sql = "INSERT INTO message (user_id, destinataire_id, message_content, message_text, message_media) 
-            VALUES (?, ?, ?, ?, ?)";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("iisss", $user_id, $destinataire_id, $message_content, $message_text, $message_media);
-
-    if ($stmt->execute()) {
-        echo "<div>Message envoyé. Vous serez redirigé dans 2 secondes...</div>";
-        echo "<script>
-                setTimeout(function(){
-                    window.location.href = 'messages.php';
-                }, 2000);
-              </script>";
-    } else {
-        echo "Erreur: " . $sql . "<br>" . $conn->error;
-    }
-
-    $stmt->close();
 }
 $conn->close();
 ?>
